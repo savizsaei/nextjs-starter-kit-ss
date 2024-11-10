d25776bcf3731aa915db171719b333c2b85ff924
aelabid <asmaa9920@hotmail.com>
2024-11-10 14:37:19 +0100

    remove GlobalError component implementation and related imports

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,6 +1,7 @@
-// app/global-error.tsx
-import React from 'react';
+// // app/global-error.tsx
+'use client';
+// import React from 'react';
 
-export default function GlobalError() {
-  return <div>An error has occurred. Please try again later.</div>;
-}
+// export default function GlobalError() {
+//   return <div>An error has occurred. Please try again later.</div>;
+// }
