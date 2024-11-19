d7c10754c93df6e886d948efbcafc9aa0b306252
aelabid <asmaa9920@hotmail.com>
2024-11-19 16:16:59 +0100

    feat: add success and warning styles to Button component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -16,6 +16,9 @@ const colorStyles = {
   primary: 'bg-blue-600 text-white border-transparent hover:bg-blue-700',
   secondary: 'bg-gray-600 text-white border-transparent hover:bg-gray-700',
   danger: 'bg-red-600 text-white border-transparent hover:bg-red-700',
+  success: 'bg-green-600 text-white border-transparent hover:bg-green-700',
+  warning: 'bg-yellow-600 text-white border-transparent hover:bg-yellow-700',
+
 };
 
 type ButtonProps = {
