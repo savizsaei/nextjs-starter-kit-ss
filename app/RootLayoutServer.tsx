df65e53f9a7704707e5d060b1dc6001dd2269524
aelabid <asmaa9920@hotmail.com>
2024-11-24 14:32:24 +0100

    feat: add Head component for improved SEO and metadata in RootLayoutServer

--- app/RootLayoutServer.tsx
+++ app/RootLayoutServer.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { getMessages } from 'next-intl/server';
 import RootLayoutClient from './RootLayoutClient';
+import Head from 'next/head';
 
 export default async function RootLayoutServer({
   children,
@@ -12,8 +13,35 @@ export default async function RootLayoutServer({
   const messages = await getMessages({ locale });
 
   return (
-    <RootLayoutClient locale={locale} messages={messages}>
-      {children}
-    </RootLayoutClient>
+    <>
+      <Head>
+        <title>Next.js Starter</title>
+        <meta
+          name="description"
+          content="A powerful and scalable Next.js starter for developers."
+        />
+        <link
+          rel="canonical"
+          href="https://next-starter-kit-neon.vercel.app/"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Best Next.js Starter" />
+        <meta
+          property="og:description"
+          content="A powerful and scalable Next.js starter for developers."
+        />
+        <meta
+          property="og:url"
+          content="https://next-starter-kit-neon.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta name="author" content="aelabid" />
+      </Head>
+      <RootLayoutClient locale={locale} messages={messages}>
+        {children}
+      </RootLayoutClient>
+    </>
   );
 }
