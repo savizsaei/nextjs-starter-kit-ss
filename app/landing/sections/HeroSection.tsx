02add61b086b57d2922f3c7d09975b4b1f7d7fdd
aelabid <asmaa9920@hotmail.com>
2024-11-26 17:27:07 +0100

    feat: enhance HeroSection component with improved layout, styling, and company logos

--- app/landing/sections/HeroSection.tsx
+++ app/landing/sections/HeroSection.tsx
@@ -1,45 +1,133 @@
 'use client';
 
-import Button from '@/components/ui/Button';
+import Image from 'next/image';
+import {
+  Container,
+  Box,
+  Heading,
+  Text,
+  Button,
+  useColorModeValue,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
+import logoLaravel from '@/public/images/logos/laravel.svg';
+import logoMirage from '@/public/images/logos/mirage.svg';
+import logoStatamic from '@/public/images/logos/statamic.svg';
+import logoStaticKit from '@/public/images/logos/statickit.svg';
+import logoTransistor from '@/public/images/logos/transistor.svg';
+import logoTuple from '@/public/images/logos/tuple.svg';
 import React from 'react';
-import { Box, Heading, Text, useColorModeValue } from '@chakra-ui/react';
 
-const HeroSection = ({ id }: { id: string }) => (
-  <Box
-    id={id}
-    className="hero-section"
-    bg={useColorModeValue('gray.50', 'gray.800')}
-    py={10}
-    px={5}
-    textAlign="center"
-  >
-    <Heading as="h1" size="2xl" mb={4}>
-      Welcome to Next.js Starter
-    </Heading>
-    <Text fontSize="lg" mb={6}>
-      Your go-to solution for fast project setups!
-    </Text>
-    <Button
-      type="button"
-      variant="solid"
-      color="blue"
-      onClick={() => window.scrollTo(0, document.body.scrollHeight)}
-      className="mb-4 md:mb-0"
+const HeroSection = ({ id }: { id: string }) => {
+  const textColor = useColorModeValue('gray.900', 'whiteAlpha.900');
+  const highlightColor = useColorModeValue('blue.600', 'blue.300');
+  const svgFillColor = useColorModeValue('blue.300', 'blue.600');
+  const subTextColor = useColorModeValue('gray.700', 'gray.300');
+
+  return (
+    <Container
+      id={id}
+      minH="100vh"
+      pb="16"
+      pt={{ base: '20', lg: '32' }}
+      textAlign="center"
+      maxW="container.lg"
     >
-      Get Started
-    </Button>
-    <Button
-      type="button"
-      variant="solid"
-      color="slate"
-      onClick={() =>
-        window.open('https://github.com/aelabid/Next-StarterKit', '_blank')
-      }
-      className="ml-0 md:ml-4"
-    >
-      View on GitHub
-    </Button>
-  </Box>
-);
+      <Heading
+        as="h1"
+        mx="auto"
+        maxW="4xl"
+        fontFamily="display"
+        fontSize={{ base: '5xl', sm: '7xl' }}
+        fontWeight="medium"
+        letterSpacing="tight"
+        color={textColor}
+      >
+        Welcome to{' '}
+        <Box
+          as="span"
+          position="relative"
+          whiteSpace="nowrap"
+          color={highlightColor}
+        >
+          <Box
+            as="svg"
+            aria-hidden="true"
+            viewBox="0 0 418 42"
+            position="absolute"
+            left="0"
+            top="2/3"
+            height="0.58em"
+            width="full"
+            fill={svgFillColor}
+            preserveAspectRatio="none"
+          >
+            <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
+          </Box>
+          <Box as="span" position="relative">
+            Next.js Starter
+          </Box>
+        </Box>
+      </Heading>
+      <Text mx="auto" mt="6" maxW="2xl" fontSize="lg" color={subTextColor}>
+        Build faster with our powerful Next.js starter! Packed with essential
+        tools like state management, React Query, I18n, CI/CD, and customizable
+        UI components, it’s everything you need to kickstart scalable,
+        high-performance projects effortlessly.
+      </Text>
+      <HStack mt="10" justify="center" spacing="6">
+        <Button
+          colorScheme="blue"
+          onClick={() => window.scrollTo(0, document.body.scrollHeight)}
+        >
+          Get Started
+        </Button>
+        <Button
+          variant="outline"
+          colorScheme="gray"
+          onClick={() =>
+            window.open('https://github.com/aelabid/Next-StarterKit', '_blank')
+          }
+        >
+          View on GitHub
+        </Button>
+      </HStack>
+      <Box mt={{ base: '36', lg: '44' }} pb="16">
+        <Text fontFamily="display" fontSize="base" color={textColor}>
+          Trusted by these six companies so far
+        </Text>
+        <VStack
+          mt="8"
+          spacing={{ base: '10', xl: '0' }}
+          align="center"
+          justify="center"
+          flexDirection={{ base: 'column', xl: 'row' }}
+        >
+          {[
+            [
+              { name: 'Transistor', logo: logoTransistor },
+              { name: 'Tuple', logo: logoTuple },
+              { name: 'StaticKit', logo: logoStaticKit },
+            ],
+            [
+              { name: 'Mirage', logo: logoMirage },
+              { name: 'Laravel', logo: logoLaravel },
+              { name: 'Statamic', logo: logoStatamic },
+            ],
+          ].map((group, groupIndex) => (
+            <HStack key={groupIndex} spacing={{ base: '12', sm: '12' }}>
+              {group.map((company) => (
+                <Box key={company.name} display="flex">
+                  <Image src={company.logo} alt={company.name} unoptimized />
+                </Box>
+              ))}
+            </HStack>
+          ))}
+        </VStack>
+      </Box>
+    </Container>
+  );
+};
 
 export default HeroSection;
