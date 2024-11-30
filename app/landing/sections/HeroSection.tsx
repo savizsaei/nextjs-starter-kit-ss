8f4361d6e0434521f386ef7f767616559e3f4cf4
aelabid <asmaa9920@hotmail.com>
2024-11-30 18:20:41 +0100

    feat: refactor HeroSection component to use Tailwind CSS and improve structure

--- app/landing/sections/HeroSection.tsx
+++ app/landing/sections/HeroSection.tsx
@@ -1,135 +1,44 @@
 'use client';
 
-import Image from 'next/image';
-import {
-  Container,
-  Box,
-  Heading,
-  Text,
-  Button,
-  useColorModeValue,
-  VStack,
-  HStack,
-} from '@chakra-ui/react';
-import logoJest from '@/public/images/logos/jest.svg';
-import logoNext from '@/public/images/logos/next.svg';
-import logoReact from '@/public/images/logos/react.svg';
-import logoRedux from '@/public/images/logos/redux.svg';
-import logoTailwind from '@/public/images/logos/tailwind.svg';
-import logoTypescript from '@/public/images/logos/typescript.svg';
+import Button from '@/components/ui/Button';
 import React from 'react';
 
 const HeroSection = ({ id }: { id: string }) => {
-  const textColor = useColorModeValue('gray.900', 'whiteAlpha.900');
-  const highlightColor = useColorModeValue('blue.600', 'blue.300');
-  const svgFillColor = useColorModeValue('blue.300', 'blue.600');
-  const subTextColor = useColorModeValue('gray.700', 'gray.300');
-
   return (
-    <Container
+    <div
       id={id}
-      minH="100vh"
-      pb="16"
-      pt={{ base: '20', lg: '32' }}
-      textAlign="center"
-      maxW="container.lg"
+      className="flex flex-col justify-center items-center min-h-screen text-center pt-16 pb-20 lg:pt-32 lg:pb-32"
     >
-      <Heading
-        as="h1"
-        mx="auto"
-        maxW="4xl"
-        fontFamily="display"
-        fontSize={{ base: '5xl', sm: '7xl' }}
-        fontWeight="medium"
-        letterSpacing="tight"
-        color={textColor}
-      >
+      <h1 className="mx-auto max-w-4xl font-medium tracking-tight text-gray-900 text-5xl sm:text-7xl mb-6">
         Welcome to{' '}
-        <Box
-          as="span"
-          position="relative"
-          whiteSpace="nowrap"
-          color={highlightColor}
-        >
-          <Box
-            as="svg"
+        <span className="relative whitespace-nowrap text-blue-600">
+          <svg
             aria-hidden="true"
             viewBox="0 0 418 42"
-            position="absolute"
-            left="0"
-            top="2/3"
-            height="0.58em"
-            width="full"
-            fill={svgFillColor}
+            className="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-300/70"
             preserveAspectRatio="none"
           >
             <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
-          </Box>
-          <Box as="span" position="relative">
-            Next.js Starter
-          </Box>
-        </Box>
-      </Heading>
-      <Text mx="auto" mt="6" maxW="2xl" fontSize="lg" color={subTextColor}>
+          </svg>
+          <span className="relative">Next.js Starter</span>
+        </span>{' '}
+      </h1>
+      <p className="mx-auto mt-6 max-w-2xl text-xl sm:text-2xl tracking-tight text-slate-700">
         Build faster with our powerful Next.js starter! Packed with essential
         tools like state management, React Query, I18n, CI/CD, and customizable
         UI components, it’s everything you need to kickstart scalable,
         high-performance projects effortlessly.
-      </Text>
-      <HStack mt="10" justify="center" spacing="6">
+      </p>
+      <div className="mt-10 flex justify-center gap-x-6">
+        <Button href="/register">Get Started</Button>
         <Button
-          colorScheme="blue"
-          onClick={() => window.scrollTo(0, document.body.scrollHeight)}
-        >
-          Get Started
-        </Button>
-        <Button
+          href="https://github.com/aelabid/Next-StarterKit"
           variant="outline"
-          colorScheme="gray"
-          onClick={() =>
-            window.open('https://github.com/aelabid/Next-StarterKit', '_blank')
-          }
         >
           View on GitHub
         </Button>
-      </HStack>
-      <Box mt={{ base: '36', lg: '44' }} pb="16">
-        <Text fontFamily="display" fontSize="base" color={textColor}>
-          Built with modern, reliable tools
-        </Text>
-        <VStack
-          mt="8"
-          spacing={{ base: '10', xl: '0' }}
-          align="center"
-          justify="center"
-          flexDirection={{ base: 'column', xl: 'row' }}
-        >
-          {[
-            [
-              { name: 'Next.js', logo: logoNext },
-              { name: 'React', logo: logoReact },
-              { name: 'TypeScript', logo: logoTypescript },
-            ],
-            [
-              { name: 'Redux', logo: logoRedux },
-              {
-                name: 'Tailwind CSS',
-                logo: logoTailwind,
-              },
-              { name: 'Jest', logo: logoJest },
-            ],
-          ].map((group, groupIndex) => (
-            <HStack key={groupIndex} spacing={{ base: '12', sm: '12' }}>
-              {group.map((tech) => (
-                <Box key={tech.name} display="flex" mx="2">
-                  <Image src={tech.logo} alt={tech.name} unoptimized />
-                </Box>
-              ))}
-            </HStack>
-          ))}
-        </VStack>
-      </Box>
-    </Container>
+      </div>
+    </div>
   );
 };
 
