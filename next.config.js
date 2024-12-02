f2f5fa53f66eae9ce1d6014f8ccbeb10b48aadfb
aelabid <asmaa9920@hotmail.com>
2024-12-02 21:45:23 +0100

    feat: add SVGR support for SVG files in webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
@@ -9,6 +10,40 @@ const nextConfig = {
   },
   experimental: {
     turbo: {}, // Enables TurboPack for faster builds
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    // Disable cache for the specific loader
+    config.cache = {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename],
+      },
+    };
+
+    return config;
   },
 };
 
