da752d466ebd0905b8173696beaabeaee36cc52e
sav <saviz.saei@gmail.com>
2025-04-02 21:40:31 -0500

    fix page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,12 +5,15 @@ import React from 'react';
 import HeroSection from './landing/sections/HeroSection';
 import FeaturesSection from './landing/sections/FeaturesSection';
 import StackSection from './landing/sections/StackSection';
-import Link from 'next/link';
-
 // import { getIntl } from '@/i18n/request';
 // Import ExampleComponent
 //import ClientWrapper from '@/components/examples/ClientWrapper';
 import ArtGallery from '../../components/ArtGallary/ArtGallery1'; // Adjusted import path
+import ComponentsSection from './landing/sections/ComponentsSection'; // Import ComponentsSection
+import DocsSection from './landing/sections/DocsSection'; // Import DocsSection
+import FooterSection from './landing/sections/FooterSection'; // Import FooterSection
+import InstallationSection from './landing/sections/InstallationSection'; // Import InstallationSection
+import TestimonialsSection from './landing/sections/TestimonialsSection'; // Import TestimonialsSection
 export default async function HomePage() {
   // const t = useTranslations('HomePage');
   // const intl = await getIntl(locale);
@@ -18,11 +21,15 @@ export default async function HomePage() {
   return (
     <>
       <main>
-
         <HeroSection id="hero" />
         <ArtGallery id="art" /> {/* Render the ArtGallery component */}
         <FeaturesSection id="features" />
         <StackSection id="stack" />
+        <ComponentsSection id="components" />
+        <DocsSection id="docs" />
+        <InstallationSection id="install" />
+        <TestimonialsSection id="testimonial" />
+        <FooterSection id="footer" /> 
       </main>
     </>
   );
